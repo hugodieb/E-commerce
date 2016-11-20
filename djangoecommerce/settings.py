54fa23e257b931339484635cde90f399e1815ac5
Hugo <hugodieb.hd@gmail.com>
2016-11-19 22:18:18 -0200

    requiremenets

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -18,9 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_dxcgbd-%wxdlmgw$l7!zt%ybpanltyxw9irvvj^j+y&2)i$1&'
 
