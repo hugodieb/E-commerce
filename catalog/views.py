e982c5b7383e658ab31f6bcc5da6fd0ba8c41b68
Hugo <hugodieb.hd@gmail.com>
2016-11-22 12:34:26 -0200

    templatetags, pagination

--- catalog/views.py
+++ catalog/views.py
@@ -8,6 +8,7 @@ class ProductListView(generic.ListView):
     model = Product
     context_object_name = 'products'
     template_name = 'catalog/products_list.html'
+    paginate_by = 3
 
 product_list = ProductListView.as_view()
 
@@ -22,6 +23,7 @@ class CategoryListView(generic.ListView):
 
     template_name = 'catalog/category.html'
     context_object_name = 'product_list'
+    paginate_by = 3
 
     def get_queryset(self):
         return Product.objects.filter(category__slug=self.kwargs['slug'])
