a98bf2f7808aee0052c2a6cd09ab24bf17cadc64
Hugo <hugodieb.hd@gmail.com>
2016-11-28 10:54:21 -0200

    login / logout view principal

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from core import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^entrar/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^sair/$', logout, {'next_page': 'index'}, name='logout'),
     url(r'^$', views.index, name='index'),
     url(r'^contato/$', views.contact, name='contact'),
     #url(r'^produto/$', views.product, name='product'),
